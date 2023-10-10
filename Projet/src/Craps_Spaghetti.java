4b2334a51deeab57798be869bf82907d6a42a998
2710emgorski <tikau2000@gmail.com>
2023-10-10 12:02:19 +0200

    rename

--- Projet/src/Craps_Spaghetti.java
+++ Projet/src/Craps_Spaghetti.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 import static java.lang.System.exit;
 
-public class TestRandom {
+public class Craps_Spaghetti {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("================================");
